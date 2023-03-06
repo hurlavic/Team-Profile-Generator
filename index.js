29ec131ac1f234e41b8083bbc47be234601ead97
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-03-06 20:40:16 +0000

    added starter files

---
+++ index.js
@@ -0,0 +1,15 @@
+const Manager = require("./__tests__/lib/Manager");
+const Engineer = require("./__tests__/lib/Engineer");
+const Intern = require("./__tests__/lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
